ce5ff5f6908142e39539a6f5f3acde0c03bed705
Johan Javier Hernández Núñez <132857486+jjhn11@users.noreply.github.com>
2025-02-12 13:38:25 -0800

    Initial Commit

---
+++ GymApp/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp
+//
+//  Created by Developer on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
