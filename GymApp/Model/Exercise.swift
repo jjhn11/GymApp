be00953eac773bfc64998da809aab6c8938e4ac7
Johan Javier Hernández Núñez <132857486+jjhn11@users.noreply.github.com>
2025-02-13 16:27:19 -0800

    Update Exercise.swift

--- GymApp/Model/Exercise.swift
+++ GymApp/Model/Exercise.swift
@@ -13,5 +13,12 @@ struct Exercise: Identifiable {
     let name: String
     let category: String
     let type: String
+    var data: [ExerciseData] = []
 }
 
+struct ExerciseData: Identifiable {
+    var id: UUID = UUID()
+    var set: Int
+    var reps: Int
+    var weight: Int
+}
